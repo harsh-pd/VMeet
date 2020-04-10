10fa6982accad7734537fe4bfe2c5b209aeda417
Harsh Priyadarshi <harsh.priyadarshi@fordi.in>
2020-04-10 17:00:50 +0530

    Calendar designed

---
+++ VMeet/Assets/Calander/scripts/CalendarDateItem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using VRExperience.UI.MenuControl;
+using UnityEngine.EventSystems;
+using VRExperience.UI;
+
+public class CalendarDateItem : MenuItem {
+
+    protected override void OnDisableOverride()
+    {
+        ToggleOutlineHighlight(false);
+        ToggleBackgroundHighlight(false);
+        if (EventSystem.current != null)
+            EventSystem.current.SetSelectedGameObject(null);
+
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        pointerHovering = true;
+        ToggleOutlineHighlight(true);
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        pointerHovering = false;
+        ToggleOutlineHighlight(false);
+        ToggleBackgroundHighlight(false);
+        if (EventSystem.current != null)
+            EventSystem.current.SetSelectedGameObject(null);
+
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        return;
+    }
+
+    public void OnDateItemClick()
+    {
+        CalendarController._calendarInstance.OnDateItemClick(gameObject.GetComponentInChildren<TextMeshProUGUI>().text);
+    }
+
+    
+}
