983032e7030201bc2e26bbb8e4969f6ef4e4eace
Harsh Priyadarshi <harsh.priyadarshi@fordi.in>
2020-04-11 18:01:23 +0530

    Desktop grid item modified

--- VMeet/Assets/Fordi/ExperienceMachine/Scripts/Core/Lobby.cs
+++ VMeet/Assets/Fordi/ExperienceMachine/Scripts/Core/Lobby.cs
@@ -18,7 +18,9 @@ namespace VRExperience.Core
 
         public override void ExecuteMenuCommand(MenuClickArgs args)
         {
-            if (args.CommandType != MenuCommandType.MEETING && args.CommandType != MenuCommandType.USER && args.CommandType != MenuCommandType.CREATE_MEETING)
+            var involvedResourceType = GetResourceType(args.CommandType);
+            
+            if (involvedResourceType != ResourceType.MEETING && args.CommandType != MenuCommandType.MEETING && args.CommandType != MenuCommandType.USER && args.CommandType != MenuCommandType.CREATE_MEETING)
                 base.ExecuteMenuCommand(args);
 
             if (args.CommandType == MenuCommandType.QUIT || args.CommandType == MenuCommandType.MAIN || args.CommandType == MenuCommandType.SETTINGS || args.CommandType == MenuCommandType.SAVE_PRESET || args.CommandType == MenuCommandType.LOBBY)
