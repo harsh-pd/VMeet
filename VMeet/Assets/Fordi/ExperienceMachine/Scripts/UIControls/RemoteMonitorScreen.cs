914b8c84af75d4f3166fe3584ed6b49cb6540c20
Harsh Priyadarshi <harsh.priyadarshi@fordi.in>
2020-04-16 16:54:04 +0530

    Ground for remote deskto monitor

---
+++ VMeet/Assets/Fordi/ExperienceMachine/Scripts/UIControls/RemoteMonitorScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using VRExperience.UI.MenuControl;
+
+namespace Fordi.ScreenSharing
+{
+    public class RemoteMonitorScreen : MenuScreen
+    {
+        [SerializeField]
+        private GameObject m_TogglePrefab;
+        [SerializeField]
+        private GameObject m_menuBorderPrefab = null;
+
+        private Toggle m_micToggle = null;
+
+        public override void OpenMenu(MenuItemInfo[] items, bool blocked, bool persist)
+        {
+            base.OpenMenu(items, blocked, persist);
+            var toggleMenu = Instantiate(m_TogglePrefab, m_contentRoot);
+            m_micToggle = toggleMenu.GetComponentInChildren<Toggle>();
+            if (m_micToggle == null)
+            {
+                Debug.LogError("Invalid m_TogglePrefab");
+                return;
+            }
+            m_micToggle.isOn = true;
+            m_micToggle.onValueChanged.AddListener(OnMicToggle);
+            toggleMenu.GetComponentInChildren<TextMeshProUGUI>().text = "Mic: ";
+            Instantiate(m_menuBorderPrefab, toggleMenu.transform);
+            toggleMenu.transform.SetSiblingIndex(0);
+        }
+
+        private void OnMicToggle(bool val)
+        {
+
+        }
+    }
+}
