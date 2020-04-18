e1a07550697a6d8760b3a1709aee456708f1aab3
Harsh Priyadarshi <harsh.priyadarshi@fordi.in>
2020-04-18 16:04:48 +0530

    TU

--- VMeet/Assets/Fordi/ExperienceMachine/Scripts/Networking/Network.cs
+++ VMeet/Assets/Fordi/ExperienceMachine/Scripts/Networking/Network.cs
@@ -83,6 +83,12 @@ namespace Fordi.Networking
         {
             base.OnJoinedLobby();
             Log("OnJoinedLobby");
+            if (PhotonNetwork.CountOfRooms > 0)
+            {
+                JoinRoom("Test");
+            }
+            else
+                CreateRoom("Test");
         }
 
         public void CreateRoom(string roomName)
