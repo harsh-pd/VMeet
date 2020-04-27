8b8d9f73245dcc12f8882c6946122c3bd4224acd
Harsh Priyadarshi <harsh.priyadarshi@fordi.in>
2020-04-27 12:51:38 +0530

    Chat element overflowing chat area - fixed

---
+++ VMeet/Assets/AppLauncher/Scripts/UI/FordiContentSizeFitter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fordi.UI
+{
+    public class FordiContentSizeFitter : ContentSizeFitter
+    {
+        [SerializeField]
+        private float m_maxWidth = 360;
+        RectTransform m_rectTransform = null;
+
+        public override void SetLayoutHorizontal()
+        {
+            base.SetLayoutHorizontal();
+            if (!m_rectTransform)
+                m_rectTransform = (RectTransform)transform;
+
+            var sizeDelta = m_rectTransform.sizeDelta;
+            if (m_maxWidth != -1 && sizeDelta.x > m_maxWidth)
+                m_rectTransform.sizeDelta = new Vector2(m_maxWidth, sizeDelta.y);
+        }
+    }
+}
