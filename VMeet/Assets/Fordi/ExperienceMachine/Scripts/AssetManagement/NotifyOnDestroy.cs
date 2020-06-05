dc4c7b0891dbd2e0f6a7d4d1c4cbc040d8829f1a
Harsh Priyadarshi <harsh.priyadarshi@fordi.in>
2020-06-05 10:42:09 +0530

    Addressables base

---
+++ VMeet/Assets/Fordi/ExperienceMachine/Scripts/AssetManagement/NotifyOnDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Fordi.AssetManagement
+{
+    internal class NotifyOnDestroy : MonoBehaviour
+    {
+        public EventHandler Destroyed;
+        private void OnDestroy()
+        {
+            Destroyed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ VMeet/Assets/Fordi/ExperienceMachine/Scripts/AssetManagement/Spawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Fordi.AssetManagement
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField]
+        private List<AssetReference> m_deps;
+
+        private readonly Dictionary<AssetReference, AsyncOperationHandle<GameObject>> m_asyncOperationHandles = new Dictionary<AssetReference, AsyncOperationHandle<GameObject>>();
+        private Dictionary<AssetReference, List<GameObject>> m_spawnedObjects = new Dictionary<AssetReference, List<GameObject>>();
+
+        private void Start()
+        {
+            foreach (var item in m_deps)
+                LoadAndSpawn(item);
+        }
+
+        private void LoadAndSpawn(AssetReference assetRef)
+        {
+            var op = Addressables.LoadAssetAsync<GameObject>(assetRef);
+
+            m_asyncOperationHandles[assetRef] = op;
+            op.Completed += (operation) =>
+            {
+                assetRef.InstantiateAsync().Completed += (asyncOpHandle) =>
+                {
+                    Debug.LogError("Spawn operation status: " + asyncOpHandle.Status.ToString());
+                    if (!m_spawnedObjects.ContainsKey(assetRef))
+                        m_spawnedObjects[assetRef] = new List<GameObject>();
+                    m_spawnedObjects[assetRef].Add(asyncOpHandle.Result);
+                    var obj = asyncOpHandle.Result;
+                    obj.AddComponent<NotifyOnDestroy>().Destroyed += (sender, args) =>
+                    {
+                        Addressables.ReleaseInstance(obj);
+                        Debug.LogError("Released: " + obj.name);
+                        m_spawnedObjects[assetRef].Remove(obj);
+                        if (m_spawnedObjects[assetRef].Count == 0)
+                        {
+                            Debug.LogError("Unloaded: " + m_asyncOperationHandles[assetRef].DebugName);
+                            Addressables.Release(m_asyncOperationHandles[assetRef]);
+                            m_asyncOperationHandles.Remove(assetRef);
+                        }
+                    };
+                };
+            };
+        }
+    }
+}
